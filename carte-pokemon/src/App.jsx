51ff4d9a347a97822a19766338394e31e18e44f6
VICTOR GUYOMARD <victorguyo44600@gmail.com>
2023-10-17 20:28:40 +0200

    Created new tab pokemon information

--- carte-pokemon/src/App.jsx
+++ carte-pokemon/src/App.jsx
@@ -1,23 +1,54 @@
-import './App.css'
-import StyledComponent from './components/styledComponent'
+import "./App.css";
+import StyledComponent from "./components/styledComponent";
 
+// class Pokemon {
+//   constructor(name, lifePoints, pokeImg, description, attackOne, strengthOne, attackTwo, strengthTwo)
+//   this.name = name,
+//   this.lifePoints = lifePoints,
+//   this.pokeImg = pokeImg,
+//   this.description = description,
+//   this.attackOne = attackOne,
+//   this.strengthOne = strengthOne,
+//   this.attackTwo = attackTwo,
+//   this.strengthTwo = strengthTwo, 
+// }
 
-// const pokemonCard = [
-//   {
-//     id: "salameche",
-//     pkmName: "Salamèche",
-//     pkmEvo: 0,
-//   },
-// ]
+// const Pokedex = {
+//   new Pokemon = {"Salamèche", "50 PV", "src/assets/salameche.jpg", "Pokémon Lézard. Taille: 0.6 m, poids: 8.5 kg", "Griffe", 10, "Flammèche", 30 }
+// }
+
+const pokemonTab = [
+  {
+    name: "Salamèche",
+    lifePoints: "50 PV",
+    pokeImg: "src/assets/salameche.jpg",
+    description: "Pokémon Lézard. Taille: 0.6 m, poids: 8.5 kg",
+    attackOne: "Griffe",
+    strengthOne: 10,
+    attackTwo: "Flammèche",
+    strengthTwo: 30,
+  },
+];
 
 function App() {
-
   return (
-    <StyledComponent />
-  )
- 
-
-
+    <>
+    
+    {pokemonTab.map((poke, index) => (
+        <StyledComponent
+          key={index}
+          name={poke.name}
+          likePoints={poke.lifePoints}
+          pokeImg={poke.pokeImg}
+          description={poke.description}
+          attackOne={poke.attackOne}
+          strengthOne={poke.strengthOne}
+          attackTwo={poke.attackTwo}
+          strengthTwo={poke.strengthTwo}
+        />
+      ))}
+    </>
+  );
 }
 
-export default App
+export default App;
--- carte-pokemon/src/components/styledComponent.jsx
+++ carte-pokemon/src/components/styledComponent.jsx
@@ -1,42 +1,67 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const Container = styled.div`
   width: 358px;
   padding: 5px 15px;
   color: black;
-  border: 17px solid #E9CC48;
+  border: 17px solid #e9cc48;
   border-radius: 14px;
 `;
 
 
-
-
-const StyledComponents = () => (
-  <Container className='pokemon-card'>
-    <h1 className='salameche base-pokemon'>Salamèche<i className='nrj nrj-feu'></i><span>50 PV</span></h1>
-    <figure>
-        <img className='pokemon-image' src='src/assets/salameche.jpg' alt='Salamèche'/>
-        <figcaption>Pokémon Lézard. Taille: 0.6 m, poids: 8.5 kg</figcaption>
-    </figure>
-    <div className='attack-block'>
-        <div className='attack attack-1 attack-only'>
-            <div className='nrj-block'><i className='nrj nrj-normal'></i></div>
-            <h2>Griffe</h2>
-            <span>10</span>
+const StyledComponent = ({name, description, attackOne, strengthOne, attackTwo, strengthTwo, lifePoints, pokeImg }) => (
+    <Container className="pokemon-card">
+      <h1 className="salameche base-pokemon">
+      {name}
+        <i className="nrj nrj-feu"></i>
+        <span>{lifePoints}</span>
+      </h1>
+      <figure>
+        <img
+          className="pokemon-image"
+          src={pokeImg}
+          alt="Salamèche"
+        />
+        <figcaption>{description}</figcaption>
+      </figure>
+      <div className="attack-block">
+        <div className="attack attack-1 attack-only">
+          <div className="nrj-block">
+            <i className="nrj nrj-normal"></i>
+          </div>
+          <h2>{attackOne}</h2>
+          <span>{strengthOne}</span>
         </div>
-        <div className='attack attack-2'>
-            <div className='nrj-block'><i className='nrj nrj-feu'></i><i className='nrj nrj-normal'></i></div>
-            <h2>Flammèche <p>Défaussez 1 carte énergie <i className='nrj nrj-feu'></i> attachée à Salamèche pour pouvoir utiliser cette attaque.</p></h2>
-            <span>30</span>
+        <div className="attack attack-2">
+          <div className="nrj-block">
+            <i className="nrj nrj-feu"></i>
+            <i className="nrj nrj-normal"></i>
+          </div>
+          <h2>
+            {attackTwo}{" "}
+            <p>
+              Défaussez 1 carte énergie <i className="nrj nrj-feu"></i> attachée
+              à Salamèche pour pouvoir utiliser cette attaque.
+            </p>
+          </h2>
+          <span>{strengthTwo}</span>
         </div>
-    </div>
-    <div className='stats-block'>
-        <h3>Faiblesse<i className='nrj nrj-eau'></i></h3>
+      </div>
+      <div className="stats-block">
+        <h3>
+          Faiblesse<i className="nrj nrj-eau"></i>
+        </h3>
         <h3>Résistance</h3>
-        <h3>Coût de retraite<i className='nrj nrj-normal'></i></h3>
-    </div>
-    <p className='infos-block'>Il préfère les endroits chauds. En cas de pluie, de la vapeur se forme autour de sa queue.   NIV.10   No.4</p>
-  </Container> 
-);
+        <h3>
+          Coût de retraite<i className="nrj nrj-normal"></i>
+        </h3>
+      </div>
+      <p className="infos-block">
+        Il préfère les endroits chauds. En cas de pluie, de la vapeur se forme
+        autour de sa queue. NIV.10 No.4
+      </p>
+    </Container>
+  );
 
-export default StyledComponents
+
+export default StyledComponent;
